458c744c3e7a547920c227e3aef0adf692efa53a
Phoebe Lau <phoebelau7@gmail.com>
2017-12-27 21:36:00 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO projects 
+
+
+INSERT INTO users(name, age) VALUES
+  ('Finnebar', 17), ('Bear', 6), ('Iguana', 4), ('Alex', 33), 
+  ('Amanda', 24), ('Sophie', 24), ('Rosey', 9), ('Victoria', 23), 
+  ('Franz', 100), ('Hermione', 30), ('Voldemort', 90), ('Marisa', 24), 
+  ('Swizzle', 4), ('Sirius', 36), ('Albus', 113), ('Squid', 5),
+  ('Whale', 6), ('Pacha', 5), ('Ena', 24), ('Katie', 24)
+
+INSERT INTO pledges
